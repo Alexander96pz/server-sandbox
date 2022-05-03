3020249877ddfcd3501b639867b5232fcd8039aa
Alexander96p <96xanderp@gmail.com>
2022-05-03 15:29:53 -0500

    implementado las reglas

--- src/main/java/com/replShell/sandbox/service/AnalisisStatic.java
+++ src/main/java/com/replShell/sandbox/service/AnalisisStatic.java
@@ -2,84 +2,223 @@ package com.replShell.sandbox.service;
 
 import com.replShell.sandbox.model.Response;
 import jdk.jshell.SnippetEvent;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import jdk.jshell.JShell;
 import jdk.jshell.VarSnippet;
 
 public class AnalisisStatic {
 
-
+    private static AnalisisStatic instancia;
+    private AnalisisStatic(){}
+    public static AnalisisStatic getinstancia(){
+        if(instancia == null){
+            return instancia = new AnalisisStatic();
+        }
+        return instancia;
+    }
     public Response CodeAnalisis(List<SnippetEvent> list, List<VarSnippet>listV, int idcode,String command){
         switch (idcode){
             case 1:
+//                caso 1
                 return AnalisisCode1(list);
+            case 2:
+//                caso 3
+                return AnalisisCode2(list,listV,command);
             case 3:
-                return AnalisisCode3(list,listV,command);
+//                caso 4:
+                return AnalisisCode4(list,listV,command);
+            case 4:
+//                caso 5:
+                return AnalisisCode5(list,listV,command);
+//            case 5:
+////                caso 6:
+//                return AnalisisCode6(list,listV,command);
             default:
                 throw new IllegalStateException("Analisis de Codigo no existente: " + idcode);
         }
     }
     public Response AnalisisCode1(List<SnippetEvent> list){
         Response obj_resp = new Response();
-            for (SnippetEvent snippet:list) {
+        for (SnippetEvent snippet:list) {
                 if (snippet.status().name() == "VALID"){
                     if( snippet.snippet().kind().name()=="STATEMENT"){
                         if(list.size()==1) {
-                            if (snippet.snippet().source().contains("System.out.print") && snippet.snippet().source().contains("\"Hola Mundo\"")) {
+                            if (snippet.snippet().source().contains("System.out.print") && snippet.snippet().source().contains("\"Hello World\"")) {
                                 obj_resp.setSource(snippet.snippet().source());
                                 obj_resp.setStatus("VALID");
                                 obj_resp.setValue(snippet.value());
                                 obj_resp.setTypeSnippet(snippet.snippet().kind().name());
                                 obj_resp.setSubtypeSnippet(snippet.snippet().subKind().name());
+                                return obj_resp;
                             }else{
-                                obj_resp=Reject(obj_resp,snippet);
+                                return Reject(obj_resp,snippet);
                             }
                         }else{
-                            obj_resp=Reject(obj_resp,snippet);
+                            return Reject(obj_resp,snippet);
                         }
                     }else{
-                        obj_resp=Reject(obj_resp,snippet);
+                        return Reject(obj_resp,snippet);
                     }
                 }else{
-                    obj_resp=Reject(obj_resp,snippet);
+                    return Reject(obj_resp,snippet);
                 }
-            }
+        }
         return obj_resp;
     }
-    public Response AnalisisCode3(List<SnippetEvent> list,List<VarSnippet> listV,String command){
+    public Response AnalisisCode2(List<SnippetEvent> list,List<VarSnippet> listV,String command){
         Response obj_resp = new Response();
-        boolean validatorVariables=false;
-        String nameVariable=null;
         if(listV.size()==1){
             for (VarSnippet vs:listV){
                 if ("VAR".equals(vs.kind().name()) && "String".equals(vs.typeName())){
-                    validatorVariables=true;
-                    nameVariable=vs.name();
+//                    nameVariable=vs.name();
+                    for (SnippetEvent snippet : list) {
+                        if(snippet.status().name().equals("VALID") && snippet.snippet().source().contains("System.out.println") && snippet.snippet().source().contains(vs.name())){
+                            obj_resp.setSource(command);
+                            obj_resp.setStatus("VALID");
+                            obj_resp.setValue(snippet.value());
+                            obj_resp.setTypeSnippet(snippet.snippet().kind().name());
+                            obj_resp.setSubtypeSnippet(snippet.snippet().subKind().name());
+                            return obj_resp;
+                        }else if(snippet.status().name().equals("REJECTED")){
+                            return Reject(obj_resp,snippet);
+                        }
+                    }
+                }else{
+                    return Reject2(obj_resp);
                 }
             }
         }
-        for (SnippetEvent snippet : list) {
-            if(snippet.status().name().equals("VALID") && snippet.snippet().source().contains("System.out.println") && snippet.snippet().source().contains(nameVariable) && validatorVariables == true){
-                obj_resp.setSource(command);
-                obj_resp.setStatus("VALID");
-                obj_resp.setValue(snippet.value());
-                obj_resp.setTypeSnippet(snippet.snippet().kind().name());
-                obj_resp.setSubtypeSnippet(snippet.snippet().subKind().name());
-                return obj_resp;
-            }else if(snippet.status().name().equals("REJECTED")){
-                return Reject(obj_resp,snippet);
+//        boolean S = listV.stream().filter(x-> "name")
+        return Reject2(obj_resp);
+    }
+    public Response AnalisisCode4(List<SnippetEvent> list,List<VarSnippet> listV,String command){
+        Response obj_resp = new Response();
+        if(listV.size()==1){
+            for (VarSnippet vs:listV){
+                if ("VAR".equals(vs.kind().name()) && "int".equals(vs.typeName())){
+                    for (SnippetEvent snippet : list) {
+                        if(snippet.status().name().equals("VALID") && snippet.snippet().source().contains("System.out.println") && snippet.snippet().source().contains(vs.name())){
+                            obj_resp.setSource(command);
+                            obj_resp.setStatus("VALID");
+                            obj_resp.setValue(snippet.value());
+                            obj_resp.setTypeSnippet(snippet.snippet().kind().name());
+                            obj_resp.setSubtypeSnippet(snippet.snippet().subKind().name());
+                            return obj_resp;
+                        }else if(snippet.status().name().equals("REJECTED")){
+                            return Reject(obj_resp,snippet);
+                        }
+                    }
+                }else{
+                    return Reject2(obj_resp);
+                }
             }
         }
 //        boolean S = listV.stream().filter(x-> "name")
-        return obj_resp;
-    }
+        return Reject2(obj_resp);
 
+    }
+    public Response AnalisisCode5(List<SnippetEvent> list,List<VarSnippet> listV,String command){
+        int cont=0;
+        Response obj_resp = new Response();
+        boolean valid=false;
+        for (SnippetEvent snippet : list) {
+            if(snippet.status().name().equals("VALID")){
+                valid=true;
+            }else{
+                return Reject2(obj_resp);
+            }
+        }
+        if(valid){
+            if(listV.size()==2){
+                for (VarSnippet vs:listV){
+                    if ("VAR".equals(vs.kind().name()) && "int".equals(vs.typeName())){
+                        cont++;
+                        if (cont==2){
+                            int suma=0;
+                            boolean valid1 =false;
+                            boolean valid2 =false;
+                            for (SnippetEvent snippet : list) {
+                                if(snippet.snippet().source().contains("System.out.println")){
+                                    valid2=true;
+                                }
+                                if(snippet.value()==""){
+                                    continue;
+                                } else {
+                                    suma=suma+Integer.parseInt(snippet.value()) ;
+                                }
+                            }
+                            if(suma == 15){
+                                valid1=true;
+                            }else {
+                                Reject2(obj_resp);
+                            }
+                            if(valid1 && valid2){
+                                obj_resp.setSource(command);
+                                obj_resp.setStatus("VALID");
+                                obj_resp.setValue(null);
+                                obj_resp.setTypeSnippet(null);
+                                obj_resp.setSubtypeSnippet(null);
+                                return obj_resp;
+                            }else{
+                                Reject2(obj_resp);
+                            }
+                        }
+                    }else{
+                        return Reject2(obj_resp);
+                    }
+                }
+            } else {
+                return Reject2(obj_resp);
+            }
+        }
+        return Reject2(obj_resp);
+    }
+//    public Response AnalisisCode5(List<SnippetEvent> list,List<VarSnippet> listV,String command){
+//        Response obj_resp = new Response();
+//        boolean validatorVariables=false;
+//        String nameVariable=null;
+//        if(listV.size()==1){
+//            for (VarSnippet vs:listV){
+//                if ("VAR".equals(vs.kind().name()) && "String".equals(vs.typeName())){
+//                    validatorVariables=true;
+//                    nameVariable=vs.name();
+//                }
+//            }
+//        }
+//        for (SnippetEvent snippet : list) {
+//            if(snippet.status().name().equals("VALID") && snippet.snippet().source().contains("System.out.print") && snippet.snippet().source().contains(nameVariable) && validatorVariables == true){
+//                obj_resp.setSource(command);
+//                obj_resp.setStatus("VALID");
+//                obj_resp.setValue(snippet.value());
+//                obj_resp.setTypeSnippet(snippet.snippet().kind().name());
+//                obj_resp.setSubtypeSnippet(snippet.snippet().subKind().name());
+//                return obj_resp;
+//            }else if(snippet.status().name().equals("REJECTED")){
+//                return Reject(obj_resp,snippet);
+//            }
+//        }
+////        boolean S = listV.stream().filter(x-> "name")
+//        return obj_resp;
+//    }
     public Response Reject(Response obj_resp,SnippetEvent snippet){
         obj_resp.setSource(snippet.snippet().source());
-        obj_resp.setStatus("REJECT");
+        obj_resp.setStatus("REJECTED");
         obj_resp.setValue(snippet.value());
         obj_resp.setTypeSnippet(snippet.snippet().kind().name());
         obj_resp.setSubtypeSnippet(snippet.snippet().subKind().name());
         return obj_resp;
     }
+    public Response Reject2(Response obj_resp){
+        obj_resp.setSource(null);
+        obj_resp.setStatus("REJECTED");
+        obj_resp.setValue(null);
+        obj_resp.setTypeSnippet(null);
+        obj_resp.setSubtypeSnippet(null);
+        return obj_resp;
+    }
+
 }
--- src/main/java/com/replShell/sandbox/service/codeAnalisis.java
+++ src/main/java/com/replShell/sandbox/service/codeAnalisis.java
@@ -2,47 +2,70 @@ package com.replShell.sandbox.service;
 
 import com.replShell.sandbox.Interface.IcodeAnalisis;
 import com.replShell.sandbox.model.Response;
-import jdk.jshell.JShell;
-import jdk.jshell.SnippetEvent;
-import jdk.jshell.SourceCodeAnalysis;
-import jdk.jshell.VarSnippet;
+import jdk.jshell.*;
+import org.apache.naming.java.javaURLContextFactory;
 import org.springframework.stereotype.Service;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 public class codeAnalisis implements IcodeAnalisis {
     private JShell shell;
     private SnippetResolver snippetResolver;
+    private AnalisisStatic as;
     public codeAnalisis() {
         snippetResolver = new SnippetResolver();
         shell = JShell.create();
+        this.as=AnalisisStatic.getinstancia();
     }
 
     @Override
     public Response onCommandEntered(String command,int idquestion) {
-        String command2=command;
-        SourceCodeAnalysis sca = shell.sourceCodeAnalysis();
-        //lista donde almacenaremos todos los fragmentos de codigo
-        List<String> snippets = new ArrayList<>();
-        //bucle para dividir el command (String) en fragmentos de codigo y almacenarlos en el lista snippets
-        do {
-            SourceCodeAnalysis.CompletionInfo info= sca.analyzeCompletion(command);
-            snippets.add(info.source());
-            //Command restante después del analisis del primer fragmento
-            command = info.remaining();
-        }while (command.length()>0);
-        List<SnippetEvent> listEvent = snippets.stream().map(shell::eval)
-                .flatMap(List::stream).collect(Collectors.toList());
-        AnalisisStatic as=new AnalisisStatic();
-        List<VarSnippet> listVar = shell.variables().collect(Collectors.toList());
-        return as.CodeAnalisis(listEvent,listVar,idquestion,command2);
+//        Desfragmento el codigo
+        List<String> snippets = DesfragmentacionSnippet(command);
+//        mapeo y evaluo el fragmento de codigo
+        List<SnippetEvent> listEvent= getListEvent(snippets);
+//        Listado de todas las variables creadas en el Entorno
+        List<VarSnippet> listVar =shell.variables().collect(Collectors.toList());
+//        Listado de todas las importaciones creadas en el Entorno
+        List<MethodSnippet> listMethods = shell.methods().collect(Collectors.toList());
+//        Listado de todas las importaciones creadas en el Entorno
+        List<ImportSnippet> listImports = shell.imports().collect(Collectors.toList());
+//        reseteo el entorno
+        limpiarEntorno();
+        return as.CodeAnalisis(listEvent,listVar,idquestion,command);
 //        Stream <Response> obj = shell.eval(command).stream()
 //                .map(snippetResolver::resolve);
 //                .reduce((prev, current) -> prev + "\n" + current)
 //                .orElse("Invalid command");
+    }
+    public List<SnippetEvent> getListEvent(List<String> snippets){
+
+        return snippets.stream().map(shell::eval)
+                    .flatMap(List::stream)
+                    .collect(Collectors.toList());
+
+    }
+    /*Metodo para dividir el command (String) en fragmentos de codigo y almacenarlos en el lista tipo String*/
+    public List<String> DesfragmentacionSnippet(String command){
+        String auxCommand=command;
+        List<String> listSnippets = new ArrayList<>();
+        SourceCodeAnalysis sca = shell.sourceCodeAnalysis();
+        do {
+            SourceCodeAnalysis.CompletionInfo info= sca.analyzeCompletion(auxCommand);
+            listSnippets.add(info.source());
+            //Command restante después del analisis del primer fragmento
+            auxCommand = info.remaining();
+        }while (auxCommand.length()>0);
+        return listSnippets;
+    }
+    public void limpiarEntorno(){
+        shell.stop();
+        shell = JShell.create();
     }
 }
